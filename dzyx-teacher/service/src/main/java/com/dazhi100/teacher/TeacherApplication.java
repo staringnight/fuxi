d64a90803222231628d0307070037678d475ab22
huangjianlou <huangjianlou@dazhi100.com>
2022-04-12 16:19:57 +0800

    初版

---
+++ dzyx-teacher/service/src/main/java/com/dazhi100/teacher/TeacherApplication.java
@@ -0,0 +1,16 @@
+package com.dazhi100.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: huangjl
+ * @date: 2022/4/12
+ */
+@SpringBootApplication
+public class TeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherApplication.class, args);
+    }
+}
