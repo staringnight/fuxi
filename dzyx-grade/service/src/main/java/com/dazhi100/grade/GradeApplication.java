fc8ab4373d37c3555a3fd9895604f363bc550280
huangjianlou <huangjianlou@dazhi100.com>
2022-04-12 10:30:15 +0800

    garde初版

---
+++ dzyx-grade/service/src/main/java/com/dazhi100/grade/GradeApplication.java
@@ -0,0 +1,16 @@
+package com.dazhi100.grade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: huangjl
+ * @date: 2022/4/12
+ */
+@SpringBootApplication
+public class GradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeApplication.class, args);
+    }
+}
